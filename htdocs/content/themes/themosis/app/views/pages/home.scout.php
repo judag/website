ce242ca983abd2426e1812d22acbfcc301251186
Julien Lambé <julien@jlambe.be>
2014-09-18 12:35:38 +0200

    Home page styles and markup.

--- htdocs/content/themes/themosis/app/views/pages/home.scout.php
+++ htdocs/content/themes/themosis/app/views/pages/home.scout.php
@@ -7,14 +7,14 @@
     <div class="wrapper">
         <h1>A framework for WordPress developers.</h1>
         <p>Build custom websites and applications with WordPress.</p>
-        <a href="#">Get started</a>
-        <img src="{{ themosis_assets() }}/images/editor.png" alt="Themosis + WordPress Editor"/>
+        <a href="#" class="primary-button">Get started</a>
+        <img id="hero-editor" src="{{ themosis_assets() }}/images/editor.png" alt="Themosis + WordPress Editor"/>
     </div>
 </div>
 <!-- End hero -->
 
 <!-- Features -->
-<div>
+<div id="features">
     <div class="wrapper">
         <h1>Features you'll love...</h1>
         <div id="features__list" class="clearfix">
@@ -44,10 +44,29 @@
             </div>
         </div>
         <h1>and more...</h1>
-        <h3>Custom post type, Metabox, Custom fields, Controllers, Models, Pages and Settings, Options, Assets, Forms, Validation, Ajax...</h3>
-        <a href="#" title="Documentation">Documentation</a>
+        <h3 id="features__others">Custom post type, Metabox, Custom fields, Controllers, Models, Pages and Settings, Options, Assets, Forms, Validation, Ajax...</h3>
+        <a class="primary-button" href="#" title="Documentation">Documentation</a>
     </div>
 </div>
 <!-- End features -->
+
+<!-- Social -->
+<div id="social">
+    <div class="wrapper">
+        <div id="social__items" class="clearfix">
+            <div id="newsletter" class="col-50 left">
+                <div>
+                    <h2>Stay tuned by signing up</h2>
+                </div>
+            </div>
+            <div id="twitter" class="col-50 left">
+                <div>
+                    <h2>Follow us on Twitter</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End social -->
 
 @stop
